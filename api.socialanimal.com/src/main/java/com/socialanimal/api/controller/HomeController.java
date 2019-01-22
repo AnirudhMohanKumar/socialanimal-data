5bb2a0aeb2feb744f6578e7cbfcdefcdddf3591d
AnirudhMohanKumar <anirudhmkumar@gmail.com>
2019-01-22 22:59:56 +0530

    First rest api

---
+++ api.socialanimal.com/src/main/java/com/socialanimal/api/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.socialanimal.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class HomeController {
+
+	@Value("${url.data}")
+	private String urlData;
+	
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
+	@GetMapping("/")
+	public String getHello() {
+		
+		RestTemplate restTemplate = new RestTemplate();
+        String dataValue = restTemplate.getForObject(urlData, String.class);
+        log.info("Hello from API \n" + dataValue);
+		return "Hello from API <br>" + dataValue;
+	}
+}
